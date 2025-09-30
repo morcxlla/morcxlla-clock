3fc4ec038456ac265f9eb05a73961427697e31f8
morcxlla <jorgecividanes2016@gmail.com>
2025-09-30 02:21:07 +0200

    Simple Optimization

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,5 @@ interface Clock {
   getClock(): string;
   nextTick(callback: (time: string) => void): void;
 }
-
 declare function createClock(): Clock;
-
 export default createClock;
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function createClock() {
   let current = new Date();
-
   function getClock() {
     current = new Date();
     const pad = (n) => String(n).padStart(2, "0");
@@ -8,17 +7,14 @@ function createClock() {
       current.getSeconds()
     )}`;
   }
-
   function nextTick(callback) {
     const now = new Date();
-    const delay = 1000 - now.getMilliseconds(); // tiempo hasta el próximo segundo
+    const delay = 1000 - now.getMilliseconds();
     setTimeout(() => {
       callback(getClock());
       nextTick(callback);
     }, delay);
   }
-
   return { getClock, nextTick };
 }
-
 export default createClock;
